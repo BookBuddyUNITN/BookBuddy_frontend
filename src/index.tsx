4b20b2f7b9f5aad3948d9a36af0c2615972c750b
stepo <StefanoPutelli>
2023-06-11 22:16:15 +0200

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout />}>
